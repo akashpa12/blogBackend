06dbb17ae0e903837b088f713cb0b9e7c0182f30
akashpa12 <akash.pa.anandan00@gmail.com>
2024-06-13 15:19:39 +0530

    token

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 const bcryptjs=require("bcryptjs") //for password encryption
+const jwt=require("jsonwebtoken")  //for token generating
 const{blogmodel}=require("./models/blogs")
 const blog=require("./models/blogs")
 
@@ -23,6 +24,41 @@ app.post("/signup",async(req,res)=>{
     let blog=new blogmodel(input)
     blog.save()
     res.json({"status":"success"})
+})
+app.post("/signin",(req,res)=>{
+    let input=req.body
+    blogmodel.find({"email":req.body.email}).then(
+        (response)=>{
+            if (response.length>0) {
+                let dbpassword=response[0].password
+                console.log(dbpassword)
+                bcryptjs.compare(input.password,dbpassword,(error,isMatch)=>{
+                    if (isMatch) {
+                        jwt.sign({email:input.email},"blog-app",{expiresIn:"1d"},(error,token)=>{
+                            if (error) {
+                                res.json({"status":"unable to generate token"})
+
+                                
+                            } else {
+                                res.json({"status":"success","userId":response[0]._id,"token":token})
+                                
+                                
+                            }
+                        })
+                        
+                        
+                    } else {
+                        res.json({"status":"incorrect"})
+                        
+                    }
+
+                })
+                
+            } else {
+                res.json({"status":"user not found"})
+            }
+        }
+    ).catch()
 })
 app.listen(8080,()=>{
     console.log("server started")
